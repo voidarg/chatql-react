f8a7753e3078e6a259f1c95fbcd21f2d24c03b5b
Mikhail Trosman <voidarg@glive.com>
2019-07-03 12:49:11 -0400

    updated chat demo

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,59 +1,86 @@
-export const ListUsers = `
-    query ListUsers {
-        listUsers {
-            items {
-                id
-                username
-                createdAt
-            }
-        }
-    }
-`
+// eslint-disable
+// this is an auto generated file. This will be overwritten
 
-export const GetUser = `
-    query GetUser($id: ID!) {
-        getUser(id: $id) {
-            id
-            username
-        }
+export const getUser = `query GetUser($id: ID!) {
+  getUser(id: $id) {
+    id
+    username
+    conversations {
+      items {
+        id
+        convoLinkUserId
+        convoLinkConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
-`
-
-export const GetUserAndConversations = `
-  query GetUserAndConversations($id:ID!) {
-    getUser(id:$id) {
+    messages {
+      items {
+        id
+        authorId
+        content
+        messageConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    createdAt
+    updatedAt
+  }
+}
+`;
+export const listUsers = `query ListUsers(
+  $filter: ModelUserFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
       id
       username
-      conversations(limit: 100) {
-        items {
-          id
-          conversation {
-            id
-            name
-          }
-        }
+      conversations {
+        nextToken
       }
-    }
-  }
-`
-
-export const GetConvo = `
-  query GetConvo($id: ID!) {
-    getConvo(id:$id) {
-      id
-      name
-      members
-      messages(limit: 100) {
-        items {
-          id
-          content
-          authorId
-          messageConversationId
-          createdAt
-        }
+      messages {
+        nextToken
       }
       createdAt
       updatedAt
     }
+    nextToken
   }
-`
+}
+`;
+export const getConvo = `query GetConvo($id: ID!) {
+  getConvo(id: $id) {
+    id
+    messages {
+      items {
+        id
+        authorId
+        content
+        messageConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    associated {
+      items {
+        id
+        convoLinkUserId
+        convoLinkConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    name
+    members
+    createdAt
+    updatedAt
+  }
+}
+`;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,35 +1,164 @@
-export const OnCreateUser = `
-    subscription OnCreateUser {
-        onCreateUser {
-            id
-            username
-            createdAt
-        }
-    }
-`
+// eslint-disable
+// this is an auto generated file. This will be overwritten
 
-export const OnCreateUserConversation = `
-  subscription OnCreateUserConversation($userId: ID!) {
-    onCreateConvoLink(convoLinkUserId:$userId) {
+export const onCreateConvoLink = `subscription OnCreateConvoLink($convoLinkUserId: ID!) {
+  onCreateConvoLink(convoLinkUserId: $convoLinkUserId) {
+    id
+    user {
       id
-      convoLinkUserId
-      convoLinkConversationId
-      conversation {
-        id
-        name
+      username
+      conversations {
+        nextToken
       }
-    }
-  }
-`
-
-export const OnCreateMessage = `
-  subscription OnCreateMessage($conversationId: ID!) {
-    onCreateMessage(messageConversationId: $conversationId) {
-      id
-      content
-      authorId
-      messageConversationId
+      messages {
+        nextToken
+      }
       createdAt
+      updatedAt
     }
+    convoLinkUserId
+    conversation {
+      id
+      messages {
+        nextToken
+      }
+      associated {
+        nextToken
+      }
+      name
+      members
+      createdAt
+      updatedAt
+    }
+    convoLinkConversationId
+    createdAt
+    updatedAt
   }
-`
+}
+`;
+export const onCreateMessage = `subscription OnCreateMessage($messageConversationId: ID!) {
+  onCreateMessage(messageConversationId: $messageConversationId) {
+    id
+    author {
+      id
+      username
+      conversations {
+        nextToken
+      }
+      messages {
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+    authorId
+    content
+    conversation {
+      id
+      messages {
+        nextToken
+      }
+      associated {
+        nextToken
+      }
+      name
+      members
+      createdAt
+      updatedAt
+    }
+    messageConversationId
+    createdAt
+    updatedAt
+  }
+}
+`;
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
+    id
+    username
+    conversations {
+      items {
+        id
+        convoLinkUserId
+        convoLinkConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    messages {
+      items {
+        id
+        authorId
+        content
+        messageConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    createdAt
+    updatedAt
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    id
+    username
+    conversations {
+      items {
+        id
+        convoLinkUserId
+        convoLinkConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    messages {
+      items {
+        id
+        authorId
+        content
+        messageConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    createdAt
+    updatedAt
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    id
+    username
+    conversations {
+      items {
+        id
+        convoLinkUserId
+        convoLinkConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    messages {
+      items {
+        id
+        authorId
+        content
+        messageConversationId
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+    createdAt
+    updatedAt
+  }
+}
+`;
